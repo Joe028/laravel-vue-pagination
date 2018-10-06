202beb9fe35c98d67edd8d8f1e4e6f5900c86526
Joe Patterson <joe@patterson.place>
2018-10-05 20:32:32 -0400

    added inner-nav slot

--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -9,6 +9,7 @@
                     </slot>
                 </a>
             </li>
+            <slot name="inner-nav"></slot>
             <li class="page-item pagination-page-nav" v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
                 <a class="page-link" href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
             </li>
