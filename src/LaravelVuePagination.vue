bcacdd6e9a2a896ddd419244479793b1617f99d7
Joe Patterson <joe@patterson.place>
2018-10-12 20:23:28 -0400

    reorganized display

--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -1,29 +1,33 @@
 <template>
     <renderless-laravel-vue-pagination :data="data" :limit="limit" v-on:pagination-change-page="onPaginationChangePage">
-        <ul class="pagination" v-if="computed.total > computed.perPage" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <li class="page-item pagination-prev-nav" v-if="computed.prevPageUrl">
-                <a class="page-link" href="#" aria-label="Previous" v-on="prevButtonEvents">
+        <div>
+            <slot name="inner-nav"></slot>
+        </div>
+        <div class="pagination" v-if="computed.total > computed.perPage" slot-scope="{ data, limit, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
+            <div class="page-item pagination-prev-nav" v-if="computed.prevPageUrl">
+                <a href="#" aria-label="Previous" v-on="prevButtonEvents">
                     <slot name="prev-nav">
                         <span aria-hidden="true">&laquo;</span>
                         <span class="sr-only">Previous</span>
                     </slot>
                 </a>
-            </li>
-            <li>
-                <slot name="inner-nav"></slot>
-            </li>
-            <li class="page-item pagination-page-nav" v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
+            </div>
+            <!--<div class="page-item pagination-page-nav" v-for="(page, key) in computed.pageRange" :key="key" :class="{ 'active': page == computed.currentPage }">
                 <a class="page-link" href="#" v-on="pageButtonEvents(page)">{{ page }}</a>
-            </li>
-            <li class="page-item pagination-next-nav" v-if="computed.nextPageUrl">
-                <a class="page-link" href="#" aria-label="Next" v-on="nextButtonEvents">
+            </div>-->
+            <div>
+                {{computed.currentPage}} of {{computed.total}}
+            </div>
+            <div class="page-item pagination-next-nav" v-if="computed.nextPageUrl">
+                <a href="#" aria-label="Next" v-on="nextButtonEvents">
                     <slot name="next-nav">
                         <span aria-hidden="true">&raquo;</span>
                         <span class="sr-only">Next</span>
                     </slot>
                 </a>
-            </li>
-        </ul>
+            </div>
+        </div>
+
     </renderless-laravel-vue-pagination>
 </template>
 
