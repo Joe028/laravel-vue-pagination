918e286c1ba86f396077b2e437983d9c6b5205ee
Joe Patterson <joe@patterson.place>
2018-12-14 16:58:22 -0500

    build

--- dist/laravel-vue-pagination.umd.js
+++ dist/laravel-vue-pagination.umd.js
@@ -921,7 +921,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"545da000-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/LaravelVuePagination.vue?vue&type=template&id=2133391e&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"545da000-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/LaravelVuePagination.vue?vue&type=template&id=7440d994&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('renderless-laravel-vue-pagination',{attrs:{"data":_vm.data,"limit":_vm.limit},on:{"pagination-change-page":_vm.onPaginationChangePage},scopedSlots:_vm._u([{key:"default",fn:function(ref){
 var data = ref.data;
 var limit = ref.limit;
@@ -929,11 +929,11 @@ var computed = ref.computed;
 var prevButtonEvents = ref.prevButtonEvents;
 var nextButtonEvents = ref.nextButtonEvents;
 var pageButtonEvents = ref.pageButtonEvents;
-return (computed.total > computed.perPage)?_c('div',{staticClass:"pagination dots-pagination"},[_c('div',[_vm._t("dots")],2),_c('div',[(computed.prevPageUrl)?_c('div',{staticClass:"page-item pagination-prev-nav"},[_c('span',_vm._g({attrs:{"aria-label":"Previous"}},prevButtonEvents),[_vm._t("prev-nav",[_c('span',{attrs:{"aria-hidden":"true"}},[_vm._v("«")]),_c('span',{staticClass:"sr-only"},[_vm._v("Previous")])])],2)]):_vm._e(),_c('div',{staticClass:"inner-nav"},[_vm._v("\n                "+_vm._s(computed.currentPage)+" of "+_vm._s(computed.lastPage)+"\n            ")]),(computed.nextPageUrl)?_c('div',{staticClass:"page-item pagination-next-nav"},[_c('span',_vm._g({attrs:{"aria-label":"Next"}},nextButtonEvents),[_vm._t("next-nav",[_c('span',{attrs:{"aria-hidden":"true"}},[_vm._v("»")]),_c('span',{staticClass:"sr-only"},[_vm._v("Next")])])],2)]):_vm._e()])]):_vm._e()}}])},[_c('div',[_vm._t("inner-nav")],2)])}
+return (computed.total > computed.perPage)?_c('div',{staticClass:"pagination dots-pagination"},[_c('div',[_vm._t("dots")],2),_c('div',[(computed.firstPageUrl)?_c('div',{staticClass:"page-item pagination-prev-nav"},[_c('span',_vm._g({attrs:{"aria-label":"Previous"}},prevButtonEvents),[_vm._t("prev-nav",[_c('span',{attrs:{"aria-hidden":"true"}},[_vm._v("«")]),_c('span',{staticClass:"sr-only"},[_vm._v("Previous")])])],2)]):_vm._e(),_c('div',{staticClass:"inner-nav"},[_vm._v("\n                "+_vm._s(computed.currentPage)+" of "+_vm._s(computed.lastPage)+"\n            ")]),(computed.nextPageUrl)?_c('div',{staticClass:"page-item pagination-next-nav"},[_c('span',_vm._g({attrs:{"aria-label":"Next"}},nextButtonEvents),[_vm._t("next-nav",[_c('span',{attrs:{"aria-hidden":"true"}},[_vm._v("»")]),_c('span',{staticClass:"sr-only"},[_vm._v("Next")])])],2)]):_vm._e()])]):_vm._e()}}])},[_c('div',[_vm._t("inner-nav")],2)])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/LaravelVuePagination.vue?vue&type=template&id=2133391e&
+// CONCATENATED MODULE: ./src/LaravelVuePagination.vue?vue&type=template&id=7440d994&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es6.number.constructor.js
 var es6_number_constructor = __webpack_require__("c5f6");
